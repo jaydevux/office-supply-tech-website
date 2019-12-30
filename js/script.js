274f57ee5f89c0d92d1b31623026b0f6861c2eaf
jaydevux <hello@jaydevux.com>
2019-12-30 17:03:21 -0500

    Adding website files

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(window).load(function() {
+    
+    
+    $("#flexiselDemo3").flexisel({
+        visibleItems: 5,
+        itemsToScroll: 1,         
+        autoPlay: {
+            enable: true,
+            interval: 4000,
+            pauseOnHover: true
+        }        
+    });
+    
+       
+});
+
+
+$(window).load(function() {
+    
+    
+    $("#flexiselDemo4").flexisel({
+        visibleItems: 4,
+        itemsToScroll: 1,         
+        autoPlay: {
+            enable: false,
+            interval: 4000,
+            pauseOnHover: true
+        }        
+    });
+    
+       
+});
+
+
+
+
+$(window).load(function() {
+    
+    
+    $("#flexiselDemo5").flexisel({
+        visibleItems: 4,
+        itemsToScroll: 1,         
+        autoPlay: {
+            enable: true,
+            interval: 4000,
+            pauseOnHover: true
+        }        
+    });
+    
+       
+});
+
